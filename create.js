8141ecce63f57d8e161bdf6d9ffb4d8e77c099a2
[Ruben <107981047+MINTYoo@users.noreply.github.com>
2024-02-11 23:31:40 -0800

    hotstuff

--- create.js
+++ create.js
@@ -14,21 +14,60 @@ function startGame(event, gameStart, getStart, choiceSection, getData) {
 
         // Get choices for the current room
         const choices = getData['start'].choices;
+        console.log(choices)
+        for (const key in choices) {
+            if (choices.hasOwnProperty(key)) { // Check if the property belongs to the object itself, not its prototype chain
+                //create new tags to display
+                const newDiv = document.createElement("div");
+                const newStrongTag = document.createElement("strong");
+                const newbutton = document.createElement("button");
+
+
+                //populate strong tag text
+                newStrongTag.textContent = choices[key]['text']
+               
+
+                //add my CSS properties
+                newStrongTag.classList.add("input"); 
+                newbutton.classList.add("input-button"); 
+                
+                //append new tags to div
+                newDiv.appendChild(newStrongTag); 
+                newDiv.appendChild(newbutton); 
+
+                //finally append the new div to the existing class
+                choiceSection.appendChild(newDiv);
+
+
+            }
+        }
+        
+        
 
         // Create HTML elements for each choice
     }
 }
 
 
+function handleNextChoice(getEnterButton, getData){
+ 
+}
+
 async function getData() {
     let gameStart = false;
     try {
         const Data = await fetch("game.json");
         const getData = await Data.json();
+        const getState = getData['start'].message
+        console.log(getState)
         const getStart = document.querySelector('.start__button'); // Corrected class name
         const choiceSection = document.querySelector('.choice__section'); // Selecting the choice__section div
         getStart.addEventListener('click', (event) => {
             startGame(event, gameStart, getStart, choiceSection, getData); // Call startGame with necessary arguments
+        });
+        const getEnterButton = document.querySelector("input-button")
+        getEnterButton.addEventListener('click', ()=>{
+            handleNextChoice(getEnterButton, getData)
         });
     } catch (err) {
         console.log(err);
